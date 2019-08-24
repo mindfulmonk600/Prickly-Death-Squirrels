87bb33d0b9821579f87008c0eccefda4f363db57
Josh O'Hagan <joshohagan17@outlook.co.nz>
2019-08-24 15:05:06 +1200

    Pushing waterfall printing feature

--- routes/waterfalls.js
+++ routes/waterfalls.js
@@ -1,8 +1,50 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  res.send('Hello world');
+router.get("/", async (req, res) => {
+  const fakeReq = {
+    radius: 12,
+    geo: {
+      lat: -41.294907,
+      long: 174.775521
+    }
+  };
+
+  const waterfallList = [
+    {
+      name: "1",
+      height: 10.0,
+      distance: 10,
+      lat: -41.294907,
+      long: 174.775521
+    },
+    {
+      name: "3",
+      height: 10.0,
+      distance: 10,
+      lat: -41.294907,
+      long: 174.775521
+    },
+    {
+      name: "2",
+      height: 10.0,
+      distance: 13,
+      lat: -41.294907,
+      long: 174.775521
+    }
+  ];
+
+  // logic 
+  const outputWaterfallList = []
+
+  for(let i=0; i<waterfallList.length; i++){
+    if (waterfallList[i].distance < fakeReq.radius){
+      outputWaterfallList.push(waterfallList[i]);
+    } 
+  }
+  res.send(outputWaterfallList);
+  res.send("ERROR");
+
 });
 
 module.exports = router;
