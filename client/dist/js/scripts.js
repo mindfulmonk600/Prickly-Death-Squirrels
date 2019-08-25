ec55039eb3dbae2acfe72cfd70db9ba17ba3595d
Lana Cleverley <cleverlana@ecs.vuw.ac.nz>
2019-08-25 13:24:43 +1200

    search button now getting local location coordinates & search box still in progress

--- client/dist/js/scripts.js
+++ client/dist/js/scripts.js
@@ -1,32 +1,46 @@
 // Gets the user location
-var x = document.getElementById("getLocation");
+var searchButton = document.getElementById("searchbutton");
+
+searchButton.addEventListener("click", getLocation)
+
+var defaultBounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(-33.8902, 151.1759),
+    new google.maps.LatLng(-33.8474, 151.2631));
+
+var input = document.getElementById('searchTextField');
+var options = {
+  bounds: defaultBounds,
+};
+
+autocomplete = new google.maps.places.Autocomplete(input, options);
 
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition, showError);
   } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
+    searchButton.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
 
 function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude +
+  searchButton.innerHTML = "Latitude: " + position.coords.latitude +
       "<br>Longitude: " + position.coords.longitude;
 }
 
 function showError(error) {
   switch(error.code) {
     case error.PERMISSION_DENIED:
-      x.innerHTML = "User denied the request for Geolocation."
+      searchButton.innerHTML = "User denied the request for Geolocation."
       break;
     case error.POSITION_UNAVAILABLE:
-      x.innerHTML = "Location information is unavailable."
+      searchButton.innerHTML = "Location information is unavailable."
       break;
     case error.TIMEOUT:
-      x.innerHTML = "The request to get user location timed out."
+      searchButton.innerHTML = "The request to get user location timed out."
       break;
     case error.UNKNOWN_ERROR:
-      x.innerHTML = "An unknown error occurred."
+      searchButton.innerHTML = "An unknown error occurred."
       break;
   }
 }
+
