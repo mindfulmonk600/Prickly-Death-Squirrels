eeb040546a2745059e06b0614e3521f376c47780
Terence Zhong <t.zhong16@gmail.com>
2019-08-25 16:28:57 +1200

    add waterFallsWithImgRout

--- routes/waterfalls.js
+++ routes/waterfalls.js
@@ -4,7 +4,7 @@ const geolib = require('geolib');
 const axios = require('axios');
 const { waterFalls } = require('../Data/nz-waterfall-points-topo-150k.js');
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
   const waterFallsProperties = waterFalls.map(wt => {
     return {
       Name: wt['properties']['Name'],
@@ -16,7 +16,31 @@ router.get('/', async (req, res) => {
     };
   });
 
-  res.send(waterFallsProperties);
+  // logic
+  const outputWaterfallList = [];
+
+  for (let i = 0; i < waterFallsProperties.length; i++) {
+    const water = waterFallsProperties[i];
+    // Gets waterfall's distance
+    const latitude = water.coordinates[1];
+    const longitude = water.coordinates[0];
+    const waterfallLocCoord = { latitude: latitude, longitude: longitude };
+    //TEST variables
+    const testRadius = 500000;
+    const userLocTest = { latitude: -41.292653, longitude: 174.777058 };
+
+    let waterfallDistance = geolib.getDistance(
+      userLocTest,
+      waterfallLocCoord,
+      1
+    );
+
+    if (waterfallDistance < testRadius) {
+      console.log(waterfallDistance);
+      outputWaterfallList.push(water);
+    }
+  }
+  res.send(outputWaterfallList);
 });
 
 module.exports = router;
