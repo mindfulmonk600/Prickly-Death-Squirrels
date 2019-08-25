f71d4ad99c892479a7afaeaa2152d141c865532c
Terence Zhong <t.zhong16@gmail.com>
2019-08-25 16:10:02 +1200

    add waterFallsWithImg route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const winston = require('winston');
 const error = require('./middleware/error');
 const waterfalls = require('./routes/waterfalls');
+const waterFallsWithImg = require('./routes/waterFallsWithImg');
 
 winston.add(new winston.transports.File({ filename: 'logfile.log' }));
 
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use('/api/waterfalls', waterfalls);
+app.use('/api/waterFallsWithImg', waterFallsWithImg);
 
 app.use(error);
 
